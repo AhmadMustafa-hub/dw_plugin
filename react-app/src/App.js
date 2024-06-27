c1527a625e5d934635e648cbc7893092d3765b00
AhmadMustafa-hub <74016414+AhmadMustafa-hub@users.noreply.github.com>
2024-06-27 20:10:51 +0300

    fix(WPCS):update the code to follow WPCS

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import apiFetch from "@wordpress/api-fetch";
 import {
   LineChart,
   Line,
@@ -10,6 +10,8 @@ import {
   ResponsiveContainer,
 } from "recharts";
 import "./App.css";
+import { SelectControl } from "@wordpress/components";
+import { __ } from "@wordpress/i18n";
 
 const App = () => {
   const [data, setData] = useState([]);
@@ -18,7 +20,7 @@ const App = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      setLoading(true); 
+      setLoading(true);
       const endDate = new Date().toISOString().split("T")[0];
       const startDate = new Date();
       startDate.setDate(startDate.getDate() - parseInt(range) + 1);
@@ -28,51 +30,50 @@ const App = () => {
         window.location.pathname.split("/wp-admin/")[0];
       const apiUrl = `${baseUrl}/wp-json/dw/v1/data`;
       try {
-        const response = await axios.get(apiUrl, {
-          params: {
-            start_date: formattedStartDate,
-            end_date: endDate,
-          },
+        const response = await apiFetch({
+          path: `${apiUrl}?start_date=${formattedStartDate}&end_date=${endDate}`,
+          headers: { "X-WP-Nonce": window.wpApiSettings.nonce },
         });
-        setData(response.data);
+        setData(response);
       } catch (error) {
         console.error("There was an error fetching the data", error);
       } finally {
-        setLoading(false);  
+        setLoading(false);
       }
     };
     fetchData();
   }, [range]);
 
-  const handleChangeRange = (e) => {
-    setRange(e.target.value);
+  const handleChangeRange = (value) => {
+    setRange(value);
   };
 
   return (
-    <div className="App" style={{ width: "100%", height: 295 }}>
-      <select
+    <div className="App">
+      <SelectControl
         value={range}
+        options={[
+          { label: __("Last 7 Days", "dw-plugin"), value: "7" },
+          { label: __("Last 14 Days", "dw-plugin"), value: "14" },
+          { label: __("Last 30 Days", "dw-plugin"), value: "30" },
+        ]}
         onChange={handleChangeRange}
-        className="filter-dropdown"
-      >
-        <option value="7">Last 7 days</option>
-        <option value="15">Last 15 days</option>
-        <option value="30">Last 30 days</option>
-      </select>
+      />
       {loading ? (
-        <p className="no-data">Loading...</p>  
+        <p>{__("Loading data...", "dw-plugin")}</p>
       ) : data.length > 0 ? (
-        <ResponsiveContainer width="100%" height="100%">
-          <LineChart
-            width={500}
-            height={300}
-            data={data.sort((a, b) => new Date(a.date) - new Date(b.date))}
-          >
-            <Line type="monotone" dataKey="value" stroke="#8884d8" />
-            <CartesianGrid stroke="#ccc" />
+        <ResponsiveContainer width="100%" height={400}>
+          <LineChart data={data}>
+            <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="date" />
             <YAxis />
             <Tooltip />
+            <Line
+              type="monotone"
+              dataKey="value"
+              stroke="#8884d8"
+              activeDot={{ r: 8 }}
+            />
           </LineChart>
         </ResponsiveContainer>
       ) : (
